c4499f098837e870e23b3af428229efc4826dcd1
Tim Mellink <Tim.mellink3@gmail.com>
2024-12-02 12:23:59 +0100

    added plot of stress distribution

--- Python/lec9_2.py
+++ Python/lec9_2.py
@@ -11,8 +11,9 @@ import Cstar_laminate as cs
 import stiffness as s
 import ABD as ab
 import alpha_star_laminate as asl
-import th_effects as th
+import thermal_effects as th
 import epsilon_star_laminate as esl
+import matplotlib.pyplot as plt
 # %% set parameters
 E1 = 128e9
 E2 = 9e9
@@ -25,6 +26,8 @@ alph = np.array([alph1, alph2, 0]) # vector of thermal coefficients in material 
 delta = 100
 layup = [0,45,90,-45]
 n = len(layup)*2
+h = t*n
+x = np.linspace(0, h, n + 1)  # height of laminate subdivided in n+1 elements
 # %% Calculate alpha star cell array
 # first calculate C, then calculate Cstar laminate
 # Then, one can calculate alpha star cell array
@@ -33,7 +36,7 @@ Cstar_lam = cs.Cstar_laminate(layup,C,n)
 z = pl.ply_edges(t,n)
 alpha_array = asl.alpha_star_laminate(alph,layup)
 # %% calculate Nth and Mth by using th_effects
-NMth = th.th_effects(Cstar_lam,delta,alpha_array,z)
+NMth = th.thermal_effects(Cstar_lam,delta,alpha_array,z)
 NMth = np.round(NMth, 7)
 print("The thermal forces Nth and Mth are: "+str(NMth))
 # %%  calculate epsilon and kappa
@@ -41,15 +44,27 @@ print("The thermal forces Nth and Mth are: "+str(NMth))
 ABD_M = ab.ABD_matrix(Cstar_lam, z)
 abd = np.linalg.inv(ABD_M)
 NM= np.array([0,0,0,0,0,0]) # no load is applied
-force = np.sum([NMth,NM],axis=0)
+force = np.sum((NMth,NM),axis=0)
 # then, calculate deformation, assuming there is no load applied
-epsk = abd@force # material CS
-epsk = np.round(epsk,4)
+epsk0 = abd@force # material CS
+epsk0 = np.round(epsk0,6)
+print("The deformations are: "+str(epsk0))
 #%% calculate stress in each layer, in ply CS
 # to do this, iterate over the layers, multiplying 
 # [C*]*({eps*}-{alpha*}*deltaT)
 # so, one needs the rotated deformation in ply CS
-eps = epsk[0:3]
-eps_star_lam = esl.epsilon_star_laminate(eps, layup)
-
-
+eps0 = epsk0[0:3]
+kappa = epsk0[3:6]
+stresses = []
+for i in range(n):
+    sig_k_b = Cstar_lam[i]@(eps0+z[i]*kappa-alpha_array[i]*delta)
+    sig_k_e = Cstar_lam[i]@(eps0+z[i+1]*kappa-alpha_array[i]*delta)
+    stresses.append([sig_k_b,sig_k_e])
+stresses = np.array(stresses)
+# %% plot
+str1 = stresses[:,:,0] # stresses in 1 direction
+str1_lie = np.reshape(str1, (-1))  # get the stresses in one row
+xr = x[::-1] # reverse the x-coordinates
+xd = [[i,i]for i in xr] # repeat the elements twice
+xd_lie = np.reshape(xd,-1) # reshape to one row
+plt.plot(str1_lie,xd_lie[1:-1]) #plot from top to bottom (only take begin and end once)
