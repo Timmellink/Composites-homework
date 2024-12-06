d9de8d23aa34789d825a46cc9e9aa2c59e703b7a
Tim Mellink <Tim.mellink3@gmail.com>
2024-12-06 16:33:02 +0100

    Refactored code

--- Python/Cstar.py
+++ Python/Cstar.py
@@ -6,5 +6,5 @@ def Cstar(C, theta):
     T = t.transformation(theta);
     Tinv = np.linalg.inv(T)
     Rinv = np.linalg.inv(R)
-    C_star = np.dot(np.dot(np.dot(np.dot(Tinv,C),R),T),Rinv);
+    C_star = np.dot(np.dot(np.dot(np.dot(Tinv,C),R),T),Rinv); 
     return C_star
--- Python/lec9_1.py
+++ Python/lec9_1.py
@@ -14,6 +14,7 @@ nu12 = 0.35
 t = 0.15e-3
 n = len(layup)*2 # number of plies
 h = t*n # height of laminate
+
 # %% To calculate ABD, calculate C for ply, then calculate C* array
 # Next, calculate z coordinates. Then, calculate ABD by ABD function
 C = s.stiffness(E1,E2,nu12,G12)
@@ -21,14 +22,17 @@ Cr = csl.Cstar_laminate(layup,C,n)
 z = pl.ply_edges(t,n)
 ABD = ab.ABD_matrix(Cr,z)
 ABD = np.round(ABD,4)
+
 # %% calculate abd, then calculate Ex, Ey, GXY using formulae
 abd =  np.linalg.inv(ABD)
 Ex = 1/(abd[0][0]*h) # Ex = 1/(a11*h) h is laminate thickness
 Ey = 1/(abd[1][1]*h) # Ey = 1/(a22*h)
 Gxy = 1/(abd[2][2]*h) # Gxy = 1/(a33*h)
+
 # %% flexular moduli 
 Efx = 12/(abd[3][3]*h**3) # Efx = 12/(d11*h^3)
 Efy = 12/(abd[4][4]*h**3) # Efy = 12/(d22*h^3)
+
 # %% print values
 print("Ex = "+str(Ex)+'\n')
 print("Ey = "+str(Ey)+'\n')
