4d750f2534874eda2c0b366b19762a27ab47a1c4
Tim <tim.mellink3@gmail.com>
2024-12-13 16:21:54 +0100

    Completed code bending stiffness

---
+++ Python/CompositeProperties.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  5 17:19:37 2024
+
+@author: Timme
+"""
+
+# module containing all relevant functions for calculation of laminate properties
+import numpy as np
+
+
+def ply_edges(h,n):
+    """
+    ply_edges returns ply top and bottom surface locations
+    
+    Parameters
+    ----------
+    h : scalar 
+        ply thickness
+    n : scalar 
+        Number of layers
+        
+    
+    Returns
+    -------
+    z : array
+        Array of length n+1 with locations of ply
+    """
+    # ply edges returns ply top and bottom surface locations
+    #
+    # Arguments:
+    #  h: ply thickness
+    #  n : number of plies
+    #
+    # Output:
+    #  z: array of length n+1 with locations of ply
+    z = np.linspace(-n*h/2,n*h/2,n+1)
+    return z
+
+
+
+def stiffness(E1,E2,nu12,G12):
+    """
+    stiffness returns C matrix
+    
+     Parameters
+     ----------
+     E1 : scalar 
+        stiffness of ply in 1 direction
+     E2 : scalar 
+        Stiffness of ply in 2 direction
+     nu12 : scalar
+         Poisson ratio
+     G12 : scalar
+         Shear modulus
+        
+    
+     Returns
+     -------
+     C : matrix
+         Stiffness matrix
+    """
+    nu21 = ((nu12*E2)/E1)
+    a11 = E1/(1-(nu12*nu21))
+    a12 = nu21*E1/(1-(nu12*nu21))
+    a21 = nu21*E1/(1-nu12*nu21)
+    a22 = E2/(1-(nu12*nu21))
+    a13, a31, a23, a32 = (0, 0, 0, 0)
+    a33 = G12
+    C = np.array([[a11, a12, a13],[a21, a22, a23],[a31, a32, a33]])
+    return C
+
+
+
+def transformation(theta):
+    """
+    transformation returns T matrix
+    
+     Parameters
+     ----------
+     theta : scalar 
+        angle of ply
+        
+    
+     Returns
+     -------
+     T : matrix
+        Transformation matrix
+    """
+    theta = theta/180*np.pi # convert to radians
+    m = np.cos(theta)
+    n = np.sin(theta)
+    a11 = m**2
+    a12 = n**2
+    a13 = 2*m*n
+    a21 = n**2
+    a22 = m**2
+    a23 = -2*m*n
+    a31 = -m*n
+    a32 = m*n
+    a33 = m**2-n**2
+    T = np.array([[a11, a12, a13], [a21, a22, a23], [a31, a32, a33]])
+    return T    
+
+
+
+def Cstar(C, theta):
+    """
+    Cstar returns C_star matrix
+    
+    Parameters
+    ----------
+    theta : scalar
+        Angle of ply
+    C : matrix
+        Stiffness matrix of ply
+        
+    
+     Returns
+     -------
+     C_star : matrix
+        Rotated Stiffness matrix
+    """
+    R=  np.array([[1, 0, 0], [0, 1, 0], [0, 0, 2]]) # Reuter matrix
+    T = transformation(theta)
+    Tinv = np.linalg.inv(T)
+    Rinv = np.linalg.inv(R)
+    C_star = Tinv@C@R@T@Rinv # C* = T^-1*C*R*T*R^-1
+    return C_star
+
+
+
+def Cstar_laminate(theta_array, C):
+    """
+    Cstar_laminate returns array of C_star matrices
+    
+    Parameters
+    ----------
+    theta_array : array
+        Array of layup 
+        (expects a symmetrical array, so only one half of the layup)
+    C : matrix
+        Stiffness matrix of one ply in material CS
+    n : scalar
+        Number of plies
+        
+    
+     Returns
+     -------
+     C_star : matrix
+        Rotated Stiffness matrix
+    """
+
+    #array = [None] * n  # set up array
+    layup = theta_array + theta_array[::-1]
+    array  = [Cstar(C,angle) for angle in layup] 
+    # fill array with C*s according to angle in symmetric layup
+  #  for angle,index in zip(layup,range(n)):
+  #      Cstar = Cstar(C, angle)
+  #      array[index] = Cstar
+    array = np.round(array,6)
+    return array
+
+
+
+def ABD_matrix(Carray, z):
+    """
+     ABD_matrix returns ABD matrix
+    
+     Parameters
+     ----------
+     Carray : list 
+         list of stiffness matrices in ply CS
+     z   : array 
+         array with location of ply edges
+    
+     Returns
+     -------
+     ABD : matrix
+         ABD matrix
+    
+    Example
+    -------
+    >>> ABD_matrix(Cr,Z)
+    [[A B], [B D]]
+    """
+    Carray = np.array(Carray) # convert list to numpy array
+    A = np.zeros((3, 3))
+    B = np.zeros((3, 3))
+    D = np.zeros((3, 3))
+    
+    if len(z) - 1 == len(Carray):
+        for i in range(len(Carray)):
+            A += Carray[i] * (z[i + 1] - z[i])
+            B += Carray[i] / 2 * (z[i + 1]**2 - z[i]**2)
+            D += Carray[i] / 3 * (z[i + 1]**3 - z[i]**3)
+    
+    AB = np.concatenate((A,B),axis=1)
+    BD = np.concatenate((B,D),axis=1)
+    ABD = np.vstack((AB, BD))
+    ABD = np.round(ABD,7)
+    return ABD
--- Python/ThreePointBending.py
+++ Python/ThreePointBending.py
@@ -9,13 +9,71 @@ Created on Thu Dec 12 16:19:13 2024
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
+import CompositeProperties as cp
+
 # %% import text file
 widths = [6, 12, 14, 13]
 df = pd.read_csv("3pb_data.txt", header=[0], sep='\s+')
 #df2.columns = ["time", "disp", "force"]
+
 # %% set variables
+t = 0.15e-3 # ply thickness
+n = 24 # number of plies
+h = t*n # laminate thickness
+Ls = 100e-3 # sample span length
+b_s = 13e-3 # sample width
+cd = {'E1' : 130e9,
+      'E2' : 10e9,
+      'G12' : 5.2e9,
+      'nu12' : 0.33,
+      'alp1' : 0.16e-6,
+      'alp' : 27.6e-6
+      } # composite data
+sd = {'S1t' : 2280e6,
+      'S1c' : 1300e6,
+      'S2t' : 110e6,
+      'S2c' : 130e6,
+      'S6' : 205e6}# strength data 
+layup1 = [45,0,-45,90]*3
+layup2 = [0]*3+[45,90,-45,-45,90,45,45,90,-45]
 
 # %% plot data
 df.plot(x="disp",y="force")
-# %% calculate stiffness
-# stiffness is 
+
+# %% calculate slope m
+df1 = df[df.disp==1] # find force at disp = 1.0
+Fd1 = df1.force.squeeze() # squeeze() to get a scalar value
+df3 = df[df.disp==3] # find force at disp = 3.0
+Fd3 = df3.force.squeeze()
+# calculate slope m = delta F/delta d
+delta_F = Fd3-Fd1 # delta F = Fd3 - Fd1
+delta_d = 3-1 # delta d = 3 - 1
+m = delta_F/delta_d 
+
+
+# %% calculate flexural stiffness
+Efs = (Ls*1e-3)**3*m/(4*b_s*1e-3*(h*1e-3)**3) # Secant stiffness is L^3m/4bh^3 (see standard) 
+
+# %% to compare flexural modulus, one has to calculate Efx
+# This can be done by calculating abd
+C = cp.stiffness(cd['E1'],cd['E2'],cd['nu12'],cd['G12']) # first, calculate the stiffness C of one ply 
+C_star_laminate = cp.Cstar_laminate(layup1, C) # Then, calculate C* laminate, based on layup (45,0,-45,90)3s
+z = cp.ply_edges(t, n) # calculate ply edges
+ABD = cp.ABD_matrix(C_star_laminate, z)# Calculate ABD matrix
+abd = np.linalg.inv(ABD) # calculate abd
+
+# %%  Find Efx by analyzing d11
+Efx = 12/(abd[3][3]*h**3) # Efx = 12/d11*h^3
+Efy = 12/(abd[4][4]*h**3) # Efy = 12/d22*h^3
+
+# %% repeat the procedure using second layup
+Cstar_lam2 = cp.Cstar_laminate(layup2,C) # first, calculate C*laminate 2
+ABD2 = cp.ABD_matrix(Cstar_lam2, z) # then calculate ABD using C*lam 2
+abd2 = np.linalg.inv(abd) # Then, calculate abd
+Efx2 = 12/(abd2[3][3]*h**3)
+Efy2 = 12/(abd2[4][4]*h**3)
+
+# %% compare the values
+Efs_m = Efs*1e-6 # flexular stiffness in MPa
+Efx_m, Efy_m =  (Efx*1e-6,Efy*1e-6) # bending stiffnesses layup1 in MPa
+Efx2_m, Efy2_m = (Efx2*1e-6,Efy2*1e-6)
