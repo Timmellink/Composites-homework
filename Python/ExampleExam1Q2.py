d087f6451843894153ccf4dc0c820aa7ebad7b0c
Tim Mellink <Tim.mellink3@gmail.com>
2025-01-27 08:49:49 +0100

    Sublime text compatibility

--- Python/ExampleExam1Q2.py
+++ Python/ExampleExam1Q2.py
@@ -6,24 +6,26 @@ Created on Fri Jan 24 16:03:00 2025
 """
 
 # %% import statements
+
 import numpy as np
 import CompositeProperties as cp
 import FailTest as ft
-
+print("test")
 # %% set properties
+
 layup = [54, -54]
 h = 4e-3
 d = 50e-2
-
 # material properties
+
 E1 = 120e9
 E2 = 9.5e9
 G12 = 3.5e9
 nu12 = 0.32
 alph1 = 1e-6
 alph2 = 30e-6
-
 # %% calculate abd
+
 C = cp.stiffness(E1, E2, nu12, G12)
 n = len(layup)*2
 t = h/n
@@ -31,8 +33,8 @@ z = cp.ply_edges(t, n)
 Cstar_lam = cp.Cstar_laminate(layup, C)
 ABD = cp.ABD_matrix(Cstar_lam, z)
 abd = np.linalg.inv(ABD)
-
 # %% determine strain values
+
 P = 50e5
 R = d/2
 a11 = abd[0][0]
@@ -49,14 +51,14 @@ eps_k = abd@NM
 epsx = eps_k[0]
 epsTheta = eps_k[1]
 print(f"The value for epsx = {epsx} and for epsTheta = {epsTheta}")
-
 # %% calculate max pressure
+
 A11 = ABD[0][0]
 A21 = ABD[1][0]
 eps1 = 0.01
 #Pmax = 2/(3*R)*eps1*(A11+A21)
-
 # %% solution max pressure
+
 a = abd[0:3,0:3]
 Re = np.array([[1,0,0],[0,1,0],[0,0,2]]) 
 Rinv = np.linalg.inv(Re)
